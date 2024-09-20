1677703a9659277a59fe6e31e10a519fc31145f6
ClaudiuKocsis <105485847+ClaudiuKocsis@users.noreply.github.com>
2024-09-20 13:19:42 +0300

    Update settings.py

--- app licenta/hotels/hotels/settings.py
+++ app licenta/hotels/hotels/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d@r2g*(o5czj$b2vc!8n&6al=xv_x7cf1v@a!9-(jun46&dp_)'
+SECRET_KEY = '...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
